95dcdae526fa4de521fc4dc0abe2689f0f33fe4e
Erick Vasquez <123427413+evgongora@users.noreply.github.com>
2025-11-01 16:01:28 -0600

    feat: semana 7 cambios

---
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.service;
+
+import com.tienda.domain.Producto;
+import com.tienda.repository.ProductoRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author erick
+ */
+@Service
+public class ProductoService {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+    
+    @Transactional(readOnly = true)
+    public List<Producto> getProductos(boolean activo) {
+        var lista = productoRepository.findAll();
+        if (activo) {
+            lista.removeIf(e -> !e.getActivo());
+        }
+        return lista;
+    }
+    
+    @Transactional(readOnly = true)
+    public Producto getProducto(Producto producto){
+        return productoRepository.findById(producto.getIdProducto()).orElse(null);
+    }
+    
+    @Transactional
+    public void save(Producto producto){
+        productoRepository.save(producto);
+    }
+    
+    @Transactional
+    public boolean deleteProducto(Producto producto){
+        try {
+            productoRepository.delete(producto);
+            productoRepository.flush();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean delete(Producto producto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
